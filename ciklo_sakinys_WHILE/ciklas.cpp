b7c080f19b61715c86fadaef355de3c85bd52a81
buyBread <buymybreadalt@gmail.com>
2021-12-08 16:51:45 +0200

    Ciklo sakinys WHILE

---
+++ ciklo_sakinys_WHILE/ciklas.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+	int num;
+	std::cout << "Įveskite bet kokį skaicių: "; std::cin >> num;
+
+	std::vector<int> digits;
+
+	while (num > 0) {
+		int digit = num % 10;
+
+		if (digit > 3 && digit < 9 && digit != 5)
+			digits.push_back(digit);
+
+		num /= 10;
+	}
+
+	std::reverse(digits.begin(), digits.end());
+
+	int prod = 1;
+
+	for (auto digit : digits) {
+		prod *= digit;
+	}
+
+	std::cout << "Skaičiaus skaitmenų kurie didesni už 3 ir mažesni už 9, bet nelygus 5 yra " << digits.size() << std::endl;
+	std::cout << "Jū sandauga: " << prod << std::endl;
+}
